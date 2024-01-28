9c0fbe75c74e80e534746537618bd260a7961579
shuklanishchal <nishchal.shukla1@gmail.com>
2024-01-27 16:20:20 -0800

    Added subfilter UI elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const el = document.querySelectorAll(".menu-item");
+for(var i = 0; i < el.length; i++) {
+  el[i].onclick = function() {
+    const actives = document.querySelectorAll(".active");
+    for(var i = 0; i < actives.length; i++) {
+      actives[i].classList.remove("active");
+    }
+    document.querySelector("#menu-selected").innerHTML = "Featured " + this.innerHTML;
+    this.classList.add("active");
+  }
+}
